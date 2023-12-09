5c2df95250412b38d42bf794ae03cfbf681450ab
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-09 10:59:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+//mutiple of 3
+let ans="";
+for(let i=1;i<=100;i++)
+{
+	if(i%3==0)
+	{
+		ans+="Fizz \n";
+	}
+	else if(i%5==0)
+		   {
+		ans+="Buzz \n"
+	}
+
+	else if(i%3==0 && i%5==0){
+		ans+="FizzBuzz \n";
+	}
+	else{
+		ans+=i+"\n";
+	}
+}
+
+alert(ans);
